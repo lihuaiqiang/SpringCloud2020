f56d7b490cadfb23a8d5570e487c8a1e41c0b621
lihuaiqiang <314207558@qq.com>
2020-12-06 20:37:39 +0800

    配置动态路由

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GatewayConfig.java
@@ -0,0 +1,35 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author lihuaiqiang
+ * @description
+ * @date 2020/12/6 17:02
+ */
+@Configuration
+public class GatewayConfig {
+
+    /**
+     * 配置了一个ID为“path_route_atguigu”的路由规则。
+     * 当访问地址 http://localhost:9527/guonei 时会自动转发到地址：http://baidu.com/guonei
+     * @param builder
+     * @return
+     */
+    @Bean
+    public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
+        RouteLocatorBuilder.Builder routes = builder.routes();
+        routes.route("path_route_atguigu", r -> r.path("/guonei").uri("http://news.baidu.com")).build();
+        return routes.build();
+    }
+
+    @Bean
+    public RouteLocator customerRouteLocator2(RouteLocatorBuilder builder) {
+        RouteLocatorBuilder.Builder routes = builder.routes();
+        routes.route("path_route_atguigu2", r -> r.path("/guoji").uri("http://news.baidu.com")).build();
+        return routes.build();
+    }
+}
